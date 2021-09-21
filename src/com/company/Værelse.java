2f6bf02c44b81cc18a803efc6c86ec452c6cc436
Kennlf <kenn741q@stud.kea.dk>
2021-09-21 10:23:26 +0200

    Renamed main

--- src/com/company/Værelse.java
+++ src/com/company/Værelse.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class Main {
+public class Værelse {
     public static void main(String[] args) {
         Lamp kitchenLamp = new Lamp(true);
         System.out.println("Køkkenlyset er: " + kitchenLamp);
